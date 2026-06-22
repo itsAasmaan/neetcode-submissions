020c05c8a1767f14ccde47e1883ba673020cfa99
Akash Singh <91951723+itsAasmaan@users.noreply.github.com>
2026-06-22 23:48:06 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int>lookup;
+
+        for (int i=0; i<nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (lookup.find(complement) != lookup.end()) {
+                return {lookup[complement], i};
+            }
+
+            lookup[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
