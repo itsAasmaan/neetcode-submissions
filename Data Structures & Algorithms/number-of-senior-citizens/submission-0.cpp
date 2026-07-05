94239d3b86ea0917bc38bdeba270b70cd57b0eb1
Akash Singh <91951723+itsAasmaan@users.noreply.github.com>
2026-07-05 22:45:13 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (const string &str: details) {
+            int age = stoi(str.substr(11, 2));
+            if (age > 60) count++;
+        }
+
+        return count;
+    }
+};
