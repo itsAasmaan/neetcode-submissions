13900d7f42e49a2d199202129849a045c6278ea7
Akash Singh <91951723+itsAasmaan@users.noreply.github.com>
2026-08-03 22:45:53 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int>result(n, 0);
+        stack<pair<int, int>>st;
+
+        st.push({temperatures[n-1], n-1});
+
+        for (int i=n-2; i>=0; i--) {
+            if (st.empty() == false) {
+                while(st.empty() == false && st.top().first <= temperatures[i]) {
+                    st.pop();
+                }
+
+                if (st.empty() == false) {
+                    result[i] = st.top().second - i;
+                }
+            }
+
+            st.push({temperatures[i], i});
+        }
+
+        return result;
+    }
+};
